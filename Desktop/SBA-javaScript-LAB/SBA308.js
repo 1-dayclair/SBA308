44bbc0002c4b1ee4f3239577065b341883a00174
super-time1 <168475235+super-time1@users.noreply.github.com>
2024-05-31 01:52:38 -0400

    SBA308

---
+++ Desktop/SBA-javaScript-LAB/SBA308.js
@@ -0,0 +1,75 @@
+let courseInfo = {
+    id: 15,
+    name: "Death Poets"  
+}; 
+
+let assignmentGroup = {
+    id: 22,
+    name: "Fashion of Anarchists",
+    courseId: 45,
+    groupWeight: 75, 
+    assignments: [
+        {
+            id: 100, 
+            name: "Intellectuals in Exile", 
+            pointsPossible: 200, 
+            dueDate: "January 17, 2057"
+        },
+        {
+            id: 201, 
+            name: "Philosophy of Anarchists",
+            pointsPossible: 150, 
+            dueDate: "February 19, 2057", 
+        },
+        {
+            id: 301,
+            name: "Strength of Economists?",
+            pointsPossible: 300,
+            dueDate: "March 18, 2057", 
+        }
+    ] 
+}; 
+
+const LearnerSubmissions = [
+    {
+        learnerId: 18,
+        assignmentId: 100,
+        submission: {
+            submittedAt: "January 16, 2057",
+            score: 197 
+        }
+    },
+    {
+        learnerId: 18,
+        assignmentId: 201, 
+        submission: {
+            submittedAt: "February 18, 2057",
+            score: 141
+        }
+    },
+    {
+        learnerId: 18, 
+        assignmentId: 301, 
+        submission: {
+            submittedAt: "March 15, 2057", 
+            score: 299
+        }
+    },
+    {
+        learnerId: 55,
+        assignmentId: 100,
+        submission: {
+            submittedAt: "January 17, 2057",
+            score: 167
+        }
+    },
+    {
+        learnerId: 55,
+        assignmentId: 201, 
+        submission: {
+            submittedAt: "March 10, 2057",
+            score: 149
+        }
+    }
+]
+
