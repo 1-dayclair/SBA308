e67c7afe40ee8f5c66bf10b2b2ad7ed813f7e20a
super-time1 <168475235+super-time1@users.noreply.github.com>
2024-06-10 19:55:03 -0400

    index.js index.html index.css

---
+++ Desktop/d-o-m/index.js
@@ -0,0 +1,20 @@
+const first = document.getElementById("start");
+
+first.appendChild(document.createElement("h1")).textContent = "SLIP!"
+first.style.color = "green";
+first.style.textAlign = "center"
+
+const image = document.createElement("img");  
+image.src = "img/IMG_5676_A.jpg";
+first.appendChild(image); 
+image.style.height = "267px"
+image.style.width = "300px"
+
+
+
+
+
+
+
+
+
